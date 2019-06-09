e334cb8d4335d36e870d7e350153914656ecfc1d
Sergey Khanov <sekhanov@gmail.com>
2019-06-09 08:39:33 +0300

    kppCam

--- agro-java-cam-utils/src/main/java/ru/skhanov/my_ipcam/App.java
+++ agro-java-cam-utils/src/main/java/ru/skhanov/my_ipcam/App.java
@@ -9,6 +9,7 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.ImageWriteException;
@@ -19,18 +20,12 @@ import org.apache.commons.imaging.formats.jpeg.exif.ExifRewriter;
 import org.apache.commons.imaging.formats.tiff.write.TiffOutputSet;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.ds.ipcam.IpCamAuth;
 import com.github.sarxos.webcam.ds.ipcam.IpCamDevice;
 import com.github.sarxos.webcam.ds.ipcam.IpCamDeviceRegistry;
 import com.github.sarxos.webcam.ds.ipcam.IpCamDriver;
 import com.github.sarxos.webcam.ds.ipcam.IpCamMode;
-
-import be.teletask.onvif.OnvifManager;
-import be.teletask.onvif.listeners.OnvifDeviceInformationListener;
-import be.teletask.onvif.listeners.OnvifServicesListener;
-import be.teletask.onvif.models.OnvifDevice;
-import be.teletask.onvif.models.OnvifDeviceInformation;
-import be.teletask.onvif.models.OnvifServices;
 
 /**
  * Hello world!
@@ -44,17 +39,20 @@ public class App {
 	
 	public static void main(String[] args) {
 		final String name = "myCam";
-		final String user = "tt"; // change to your own username
-		final String pass = "samsung"; // change to your own password
-		final String url = "http://192.168.100.41:8080/photo.jpg"; // camera's IP address or domain
+		final String user = "admin"; // change to your own username
+		final String pass = ""; // change to your own password
+//		final String user = "tt"; // change to your own username
+//		final String pass = "samsung"; // change to your own password
+		final String url = "http://192.168.100.188/video1.mjpg"; // camera's IP address or domain
+//		final String url = "http://192.168.100.41:8080/photo.jpg"; // camera's IP address or domain
 		
 		try {
-			IpCamDeviceRegistry.register(new IpCamDevice(name, url, IpCamMode.PULL, new IpCamAuth(user, pass)));
+			IpCamDeviceRegistry.register(new IpCamDevice(name, url, IpCamMode.PUSH, new IpCamAuth(user, pass)));
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		final Webcam webcam = Webcam.getDefault();
+		Webcam webcam = Webcam.getWebcams().get(0);
 		webcam.open();
 		BufferedImage image = webcam.getImage();
 		try {
@@ -62,9 +60,8 @@ public class App {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		System.out.println("complete");
-		webcam.close();
+		}	
+		
 		
 		try {
 			File inputFile = new File("test.jpg");
@@ -86,17 +83,17 @@ public class App {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		webcam.close();
 		
 		
 		
+		WebcamPanel panel = new WebcamPanel(webcam);		
+		JFrame f = new JFrame(name);
+		f.add(panel);
+		f.pack();
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		
-//		final WebcamPanel panel = new WebcamPanel(webcam);
-//		
-//		JFrame f = new JFrame(name);
-//		f.add(panel);
-//		f.pack();
-//		f.setVisible(true);
-//		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}	
 
 }
